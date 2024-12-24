5fa61650b8089d0a9dcad3dd73cf07d5da7defdc
bonsa baker <bakerboonsa@gmail.com>
2024-12-24 20:15:47 +0300

    vercel

--- CAR-RANTING/vite.config.js
+++ CAR-RANTING/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: [
-        './', // Allow your project root directory
-        '/home/bonsa/BIGPROJECT/FRONT-CARRAN/node_modules/@fortawesome', // Allow FontAwesome webfonts
-      ],
-    },
+  build: {
+    outDir: 'dist', // Ensure the build output goes to the "dist" folder
   },
-})
+});
